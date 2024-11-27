3e45ae22d1d3d5eb1e0d6c2f96a65a809ba6a4a4
botwalain <167398125+botwalain@users.noreply.github.com>
2024-11-27 18:16:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -70,8 +70,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want no results messages in Log Channel
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vicky5:2003@cluster0.djc61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "vicky5")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ibm:2003@cluster0.ufkqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "ibm")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'ibmbotsupport')
 
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
